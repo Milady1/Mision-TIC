403cf41928bcd23be62803916d5f6ecb68731a1f
Milady1 <mmachado05@misena.edu.co>
2021-10-01 17:08:36 -0500

    Vista de detalles,conexion con persistencia

--- ProyectoCiclo3.App.Dominio/Entidades/Aviones.cs
+++ ProyectoCiclo3.App.Dominio/Entidades/Aviones.cs
@@ -3,10 +3,10 @@ namespace ProyectoCiclo3.App.Dominio{
     public class Aviones{
         public int id { get; set; }
         public string Marca{ get; set; }
-        public string Modelo { get; set; }
+        public int Modelo { get; set; }
         public int numero_asientos { get; set; }
         public int numero_baños{ get; set; }
-        public int Capacidad_maxina { get; set; }
+        public int Capacidad_maxima { get; set; }
     }
 
 }
