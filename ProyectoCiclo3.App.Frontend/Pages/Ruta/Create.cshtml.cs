237948fcb187dc471d1720a793c1e1c051a04015
Milady1 <mmachado05@misena.edu.co>
2021-10-06 23:08:11 -0500

    second commit

--- ProyectoCiclo3.App.Frontend/Pages/Ruta/Create.cshtml.cs
+++ ProyectoCiclo3.App.Frontend/Pages/Ruta/Create.cshtml.cs
@@ -4,14 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProyectoCiclo3.App.Persistencia.AppRepositorios;
+using ProyectoCiclo3.App.Dominio;
  
 namespace ProyectoCiclo3.App.Frontend.Pages
 {
-    public class FormRutaModel : PageModel
+    public class EditRutaModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly RepositorioRutas repositorioRutas;
+        [BindProperty]
+        public Rutas Ruta {get;set;}
  
+        public EditAvionModel(RepositorioRutas repositorioRutas)
+       {
+            this.repositorioRutas=repositorioRutas;
+       }
+
+          public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }            
+            Ruta = repositorioRutas.Create(Ruta);
+            return RedirectToPage("./List");
         }
     }
 }
